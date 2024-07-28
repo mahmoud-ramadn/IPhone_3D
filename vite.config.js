ab36b0f5a530cc8e04b14c82ead190c2077650ba
mahmoud-ramadn <mr9026573@gmail.com>
2024-07-28 18:48:49 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "home-1tl",
-    project: "javascript-react"
-  })],
-
+  plugins: [react()],
   build: {
-    sourcemap: true
-  }
-})
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Suppress specific warnings
+        if (warning.code === 'SOME_WARNING_CODE') return;
+        // Use default for everything else
+        warn(warning);
+      },
+    },
+  },
+});
